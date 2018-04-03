f29be38c94216c40d26f168381e9ecdc219e1758
Sidis, Itay (is874k) <is874k@intl.att.com>
2018-04-03 17:20:58 +0300

    create-component-app custom template

---
+++ client/templates/ts-component/COMPONENT_NAME.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import COMPONENT_NAMEProps from './COMPONENT_NAMEProps';
+import COMPONENT_NAMEState from './COMPONENT_NAMEState';
+import './COMPONENT_NAME.css';
+
+export default class COMPONENT_NAME extends React.Component<COMPONENT_NAMEProps, COMPONENT_NAMEState> {
+    render() {
+        return (<div>COMPONENT_NAME Component</div>);
+    }
+}
---
+++ client/templates/ts-component/COMPONENT_NAMEProps.ts
@@ -0,0 +1,3 @@
+export default interface COMPONENT_NAMEProps {
+  
+}
